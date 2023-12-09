2db8e39fe703490807232ef05ab715af4d619636
kenzocarneiro <kenzo.carneiro.36@gmail.com>
2023-12-09 15:35:00 +0100

    Added Inital Spring Boot Project

---
+++ src/main/java/fr/insacvl/asl/bcn/bookzone/BookzoneApplication.java
@@ -0,0 +1,13 @@
+package fr.insacvl.asl.bcn.bookzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/insacvl/asl/bcn/bookzone/BookzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insacvl.asl.bcn.bookzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
