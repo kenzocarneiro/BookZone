dfecf4b988221f3ad500bf15c202947deb5f114f
mylane-nguyen <mylane.nguyen@gmail.com>
2023-12-14 22:09:34 +0100

    add sql script for database test

---
+++ src/main/resources/insert-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Personne(ID, NOM, PRENOM) VALUES (1, 'Hugo', 'Victor'),(2, 'De la Fontaine', 'Jean'), (3, 'Camus', 'Albert');
+INSERT INTO Utilisateur(ID, MAIL, LOGIN, PASSWORD) VALUES (1, 'jacques@gmail.com', 'jacques', 'jj'),(2, 'henri@laposte.net', 'henri', 'irneh'), (3, 'micheline@yahoo.fr', 'mimi', 'mimi123');
+INSERT INTO Administrateur(ID, MAIL, LOGIN, PASSWORD) VALUES (1, 'root@free.fr', 'root', 'root123');
