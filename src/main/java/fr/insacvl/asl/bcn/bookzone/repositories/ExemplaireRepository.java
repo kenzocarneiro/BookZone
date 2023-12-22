48ee202af776bc73185772cf9571aab50f9d9810
nicos-code <nicos01code@gmail.com>
2023-12-22 22:58:51 +0100

    Add libraire and other infos to index.html and exemplaire.html Display only on index.html when EtatLivraisonExemplaire is EN_VENTE

--- src/main/java/fr/insacvl/asl/bcn/bookzone/repositories/ExemplaireRepository.java
+++ src/main/java/fr/insacvl/asl/bcn/bookzone/repositories/ExemplaireRepository.java
@@ -1,9 +1,13 @@
 package fr.insacvl.asl.bcn.bookzone.repositories;
 
+import fr.insacvl.asl.bcn.bookzone.entities.EtatLivraisonExemplaire;
 import fr.insacvl.asl.bcn.bookzone.entities.Exemplaire;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExemplaireRepository extends JpaRepository<Exemplaire, Integer> {
     // List<Exemplaire> findByOuvrage(Integer idOuvrage);
+    List<Exemplaire> findByEtatLivraisonExemplaire(EtatLivraisonExemplaire etatLivraisonExemplaire);
 }
